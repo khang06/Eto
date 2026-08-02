0f3062054c026755f526a47981b5a956557387bb
Khang <khang06@users.noreply.github.com>
2026-08-02 00:33:23 -0400

    Make build.sh compile as release too

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-dotnet msbuild /v:minimal /t:Package /p:SetVersion=$1 $DIR/build/Build.proj
+dotnet msbuild /v:minimal /t:Package /p:SetVersion=$1 -property:Configuration=Release  $DIR/build/Build.proj
